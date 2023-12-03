a04d74df2438f8a797bd1b6612fc67980ab097af
Dzianis Dashkevich <dskecse@gmail.com>
2023-12-03 16:41:49 +0300

    [Day 1] Add solution to Day 1 Part 2

--- day1/part2/solution_test.rb
+++ day1/part2/solution_test.rb
@@ -3,7 +3,7 @@ require_relative "solution"
 
 class TestCalibrationDocument < Minitest::Test
   def setup
-    @calibration_document = CalibrationDocument.new(file: "example.txt")
+    @calibration_document = CalibrationDocument.new(file: "input.txt")
   end
 
   def test_calibration_values_is_initially_an_empty_array
@@ -15,10 +15,10 @@ class TestCalibrationDocument < Minitest::Test
   end
 
   def test_recovers_calibration_document
-    assert_equal [29, 83, 13, 24, 42, 14, 76, 35, 52, 91, 21, 52], @calibration_document.recover.calibration_values
+    refute_empty @calibration_document.recover.calibration_values
   end
 
   def test_returns_the_valid_sum_of_calibration_values
-    assert_equal 532, @calibration_document.recover.calibration_values.sum
+    assert_equal 54249, @calibration_document.recover.calibration_values.sum
   end
 end
