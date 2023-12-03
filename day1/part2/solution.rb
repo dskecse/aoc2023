d02010f7f2e475d6eed9f1dec9e6f7400894b8f3
Dzianis Dashkevich <dskecse@gmail.com>
2023-12-03 16:07:25 +0300

    Fix example solution to Part Two

--- day1/part2/solution.rb
+++ day1/part2/solution.rb
@@ -11,8 +11,8 @@ class CalibrationDocument
       first_word = find_first_digit_as_word(line)
       line.sub!(first_word, words_to_digits) if first_word
 
-      last_word = find_last_digit_as_word(line)
-      line.sub!(last_word, words_to_digits) if last_word
+      last_word, index = find_last_digit_as_word_with_index(line)
+      line[index, last_word.size] = words_to_digits[last_word].to_s if last_word
 
       digit_chars = line.delete("^0-9").chars
       @calibration_values << (digit_chars[0] + digit_chars[-1]).to_i
@@ -41,14 +41,14 @@ class CalibrationDocument
     first_word
   end
 
-  def find_last_digit_as_word(line)
+  def find_last_digit_as_word_with_index(line)
     max_index, last_word = -1, nil
     words_to_digits.each_key do |word|
-      if (index = line.index(word)) && index > max_index
+      if (index = line.rindex(word)) && index > max_index
         max_index = index
         last_word = word
       end
     end
-    last_word
+    [last_word, max_index]
   end
 end
